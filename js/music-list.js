991418dc7c9af05ecf0ee640f8c7aa569d2472d1
Ignacio Chavez <nacho_ch3@hotmail.com>
2023-07-24 16:46:46 -0400

    Primer commit

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name:"505",
+        artist:"Arctic Monkeys",
+        img:"art1",
+        src:"music-1"
+    },
+    {
+        name:"Imagination",
+        artist:"Foster the people ",
+        img:"art2",
+        src:"music-2"
+    },
+    {
+        name:"Be Sweet",
+        artist:"Japanese Breakfast",
+        img:"art3",
+        src:"music-3"
+    },
+    {
+        name:"No Buses",
+        artist:"Arctic Monkeys",
+        img:"art4",
+        src:"music-4"
+    },
+    {
+        name:"Duality",
+        artist:"Slipknot",
+        img:"art5",
+        src:"music-5"
+    },
+]
